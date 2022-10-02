56694b395345c1cd7349bba72d313adb249b1ee5
Simran <simran.debnath@kcl.ac.uk>
2022-10-03 00:25:04 +0100

    "Prime number methods"

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    num = 2
+    count = 1
     list = []
+    while count <= number_of_primes:
+        prime = True
+        for i in range(2, num):
+            if (num % i) == 0:
+                prime = False
+                break
+        if prime:
+            list.append(num)
+            count += 1
+        num += 1
+
     return list
+  
+    
